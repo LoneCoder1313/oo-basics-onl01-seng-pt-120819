e0862387bc8390b258678549b869f5b48e4ad37a
Ricky dildy <rdildy92@gmail.com>
2020-01-10 02:12:20 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,34 +1,3 @@
-# class Book
-#   def initalize (title)
-#     @title = title
-#   end 
-  
-#   def title 
-#     @title
-#   end 
-#   def author = (author)
-#     @author = author
-#   end 
-#   def author
-#     @author
-#   end 
-#   def page_count = (num)
-#     @page_count = num
-#   end 
-#   def page_count
-#     @page_count
-#   end 
-#   def genre=(genre)
-#     @genre = genre
-#   end
-
-#   def genre
-#     @genre
-#   end
-#   def turn_page
-#     puts "Flipping the page...wow, you read fast!"
-#   end
-# end 
 
 class Book
   attr_accessor :author, :page_count, :genre
@@ -43,3 +12,5 @@ class Book
   end
 
 end
+ 
+ 
